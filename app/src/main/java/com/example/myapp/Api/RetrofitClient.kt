784b08f57aaf7018105f2d3d4388659e45cec118
Fanash Beauty <pathanhuda80@gmail.com>
2024-10-05 16:01:38 +0530

    "Auth changed"

--- app/src/main/java/com/example/myapp/Api/RetrofitClient.kt
+++ app/src/main/java/com/example/myapp/Api/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private const val BASE_URL = "https://api.salequick.com/v2/login_v3_api/"
-    private val AUTH = "Basic "+ Base64.encodeToString("developer@salequick.com:Developer@#$343".toByteArray(), Base64.NO_WRAP)
+    private val AUTH = "Basic "+ Base64.encodeToString("X-Requested-With:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZXJjaGFudF9pZCI6IjQxMyJ9.zRIu9DQXJjIKaFdUzbQvbBH1bbzZzyA0tiO0nXS3VI8".toByteArray(), Base64.NO_WRAP)
 
 
     private val okHttpClient = OkHttpClient.Builder()
